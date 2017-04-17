7fc4034e84dcd06d1187e10dad620e26ce08cca1
claudezhang <11610606@mail.sustc.edu.cn>
2017-04-17 21:08:42 +0800

    增加了添加乘客 功能
    增加了一个方法，用来向 飞机乘客列表 中添加乘客

--- Role/Passenger/PassengerOfTheFlight.java
+++ Role/Passenger/PassengerOfTheFlight.java
@@ -1,8 +1,23 @@
 package Passenger;
 
+import java.util.ArrayList;
+
 public class PassengerOfTheFlight {
 	int MaxPassenger;
-	static Passenger_ind[] List;
+	ArrayList<Passenger_ind> List;
+	int CurrentNumber = List.size();
+	
+//	Add a passenger to flight
+	public void Add(PassengerOfTheFlight PLF, Passenger_ind newguy){
+		if (CurrentNumber < MaxPassenger){
+			PLF.List.add(newguy);
+			System.out.println("Successfully add new passenger!!!");
+		}
+		else{
+			System.out.println("There is no seat on this flight!!!");
+		}
+	}
+	
 	
 
 }
