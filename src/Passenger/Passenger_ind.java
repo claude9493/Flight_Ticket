98d774807b9d39ff452fa3cc6b7eb058653d2e3f
claudezhang <11610606@mail.sustc.edu.cn>
2017-05-04 00:44:44 +0800

    重写Passenger部分代码
    变更不小。

--- src/Passenger/Passenger_ind.java
+++ src/Passenger/Passenger_ind.java
@@ -6,9 +6,28 @@ import Order.Order;
 
 class Passenger_ind {
 	public int passagerID;
-	String realName;
-	String identityID;
-	String password;
-	ArrayList<Order> orderlist;
-	enum state{REGISTERED, UNREGISTERED};
+	public String realName;
+	public String identityID;
+	private String password;
+	public ArrayList<Order> myorder;
+
+	enum status_list {
+		ONLINE, OFFLINE
+	};
+	status_list status = status_list.OFFLINE;
+
+	public void set_password(String password) {
+		this.password = password;
+	}
+
+	public String get_password() {
+		return this.password;
+	}
+	
+	public void change_status(){
+		if(this.status == status_list.OFFLINE)
+			status = status_list.ONLINE;
+		else
+			status = status_list.OFFLINE;
+	}
 }
--- src/Passenger/passenger.java
+++ src/Passenger/passenger.java
@@ -1,44 +1,90 @@
 package Passenger;
+/**
+ * This class stores the information of all the passengers 
+ * admins can use the method provided to manage the passengers
+ */
 
-class passenger {
-	
-	static final int MAXLEN = 100;
-	Passenger_ind[] PassengerList = new Passenger_ind[MAXLEN+1];
-	int ListLen;
-	
-	void Inint(passenger PL){
-		this.ListLen = 0;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class passenger {
+
+	public final int MAXLEN = 100;
+	public ArrayList<Passenger_ind> PassengerList = new ArrayList<Passenger_ind>();
+
+	// Register and store the information of the new passenger to the file
+	public void register() throws Exception {
+		Scanner input = new Scanner(System.in);
+		Passenger_ind new_passenger = new Passenger_ind();
+		System.out.println("REGISTER");
+		System.out.print("UserName:\t");
+		new_passenger.identityID = input.nextLine().trim();
+		System.out.print("\nPassword:\t");
+		new_passenger.set_password(input.nextLine().trim());
+		PassengerList.add(new_passenger);
+		write(new_passenger);
+		input.close();
+		System.out.println("\nCongratulations, you have successfully registered.");
 	}
-	
-	int passengerLength(passenger PL){
-		return PL.ListLen;
-				
-	}
-	
-	int PLAdd(passenger PL, Passenger_ind new_passenger){
-		if(PL.ListLen>=MAXLEN){
-			System.out.print("No more space");
-			return 0;
-		}
-		PL.PassengerList[++PL.ListLen] = new_passenger;
-		return 1;
-	}
-	
-	int PLDelete(passenger PL, int n){
-		int i;
-		if(n < 1|| n > PL.ListLen+1){
-			System.out.print("Wrong index.");
-			return 0;
-		}
-		for (i = n; i < PL.ListLen; i ++){
-			PL.PassengerList[i] = PL.PassengerList[i+1];
+
+	// LOG
+	public void log() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Your identityID:\t");
+		String ID = input.nextLine().trim();
+		System.out.print("\nPassword:\t");
+		String password = input.nextLine().trim();
+		input.close();
+		if (check(ID) == -1)
+			System.out.println("\nSuch passenger doesn't exist in system.");
+		else if (!PassengerList.get(check(ID)).equals(password))
+				System.out.println("Wrong password, please try again.\n");
+		else{
+			PassengerList.get(check(ID)).change_status();
+			System.out.println("\nCongratulations, you have successfully loged in.");
 		}
 		
-		PL.ListLen--;
-		return 1;
+
 	}
-	
-	
-	
-	
+
+	// Delete a passenger from the list and the file
+	public void PLDelete(int n) {
+		if (n < 1 || n > this.PassengerList.size()) {
+			System.out.print("Wrong index.");
+		} else {
+			this.PassengerList.remove(n);
+		}
+	}
+
+	public int check(String name) {
+		for (Passenger_ind p : PassengerList) {
+			if (p.identityID.equals(name))
+				return PassengerList.indexOf(p);
+		}
+		return -1;
+	}
+
+	// read the passengers'information from the file
+	public void read() throws Exception {
+		Scanner input = new Scanner(new File("Passengers.txt"));
+		Passenger_ind np = new Passenger_ind();
+		while (input.hasNext()) {
+			np.passagerID = input.nextInt();
+			np.identityID = input.next();
+			np.set_password(input.next());
+			np.realName = input.next();
+			PassengerList.add(np);
+		}
+		input.close();
+	}
+
+	// write information to the file
+	public void write(Passenger_ind p) throws Exception {
+		Formatter output = new Formatter("Passengers.txt");
+		output.format("%d\t%s\t%s\t%s\t", p.passagerID, p.identityID, p.get_password(), p.realName);
+		output.close();
+	}
+
 }
