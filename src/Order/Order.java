3567273b4a67ce13cb891c2142720c552223399b
claudezhang <11610606@mail.sustc.edu.cn>
2017-05-08 02:22:06 +0800

    变量重命名

--- src/Order/Order.java
+++ src/Order/Order.java
@@ -2,19 +2,23 @@ package Order;
 
 import java.util.Date;
 
+import Passenger.Passenger_ind;
 import Flight.Flight_ind;
 
 public class Order {
+	public int OrderID;
 	public int passengerID;
-	public String seat;
+	public int seat;
 	public Flight_ind flight;
 	public Date createDate;
 	public enum StatusList{UNPAID,PAID,CANCEL};
 	public StatusList status;
 	
-	public void makeorder(int ID)
+	public Order(Flight_ind f, Passenger_ind p)
 	{
-		
+		this.flight = f;
+		this.passengerID = p.passagerID;
+		this.status = StatusList.UNPAID;
 	}
 
 }
